aedc75bd499a809185703bea6569465fe50ee616
Aman Gupta <kramangupta8552@gmail.com>
2025-12-04 11:56:08 +0530

    first commit

---
+++ angular-demo1/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { log } from 'node:console';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Login, Signup],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('angular-demo1');
+}
