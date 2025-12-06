aee5449e4a53efae70df42838e641033d11a48ae
Aman Gupta <kramangupta8552@gmail.com>
2025-12-06 11:09:42 +0530

    else_if

--- angular_demo2/src/app/app.ts
+++ angular_demo2/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GetSet } from './get-set/get-set';
 import { IfElse } from './if-else/if-else';
+import { ElseIf } from './else-if/else-if';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GetSet, IfElse],
+  imports: [RouterOutlet, GetSet, IfElse, ElseIf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
